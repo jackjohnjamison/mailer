9931c29153ba94feff08ac331b6fdc7f841001e2
CLARENCE\john.jamison <jackofthejamison@hotmail.com>
2021-01-17 14:04:23 +0000

    Reorganisation

---
+++ app.js
@@ -0,0 +1,31 @@
+const  express = require('express')
+const  path = require('path')
+const  cookieParser = require('cookie-parser')
+const  logger = require('morgan')
+const  nunjucks = require('nunjucks')
+
+const  indexRouter = require('./routes/index')
+const  mailerRouter = require('./routes/mailer')
+
+const  app = express()
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
+
+// configute Nunjucks with 'views' as templates directory
+nunjucks.configure('views', {
+  autoescape:  true,
+  express:  app
+})
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended:  false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+app.use('/mailer', mailerRouter)
+
+module.exports = app
---
+++ readme.md
@@ -0,0 +1,3 @@
+https://www.smashingmagazine.com/2020/04/express-api-backend-project-postgresql/
+
+https://www.digitalocean.com/community/tutorials/use-expressjs-to-get-url-and-post-parameters
