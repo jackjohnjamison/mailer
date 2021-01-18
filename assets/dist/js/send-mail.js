2ff67cdad232e299a4c39173b85a889bd67059cf
CLARENCE\john.jamison <jackofthejamison@hotmail.com>
2021-01-18 14:06:10 +0000

    JS and layout tweaks

--- assets/dist/js/send-mail.js
+++ assets/dist/js/send-mail.js
@@ -1,37 +1,43 @@
-function postRequest(emailDataJSON) {
-    const request = new XMLHttpRequest()
-    const API_URL = 'http://localhost:3000/mailer' // TODO enviroment variable
-
-    request.open("POST", API_URL, true)
-    request.setRequestHeader("Content-type", "application/json")
-    request.send(emailDataJSON)
-
-    request.onreadystatechange = () => {
-        console.log(request.readyState, request.status, request.response)
+(() => {
+    function postRequest(emailDataJSON) {
+        const request = new XMLHttpRequest()
+        const API_URL = 'http://localhost:3000/mailer' // TODO enviroment variable
+    
+        request.open("POST", API_URL, true)
+        request.setRequestHeader("Content-type", "application/json")
+        request.send(emailDataJSON)
+    
+        request.onreadystatechange = () => {
+            console.log(request.readyState, request.status, request.response)
+        }
     }
-}
-
-function collectEmailData() {
-    return {
-        toAddress: document.getElementById('toAddress').value,
-        fromAddress: document.getElementById('fromAddress').value,
-        subject: document.getElementById('subject').value,
-        html: document.getElementById('html').value,
-        text: document.getElementById('text').value
+    
+    function collectEmailData() {
+        return {
+            toAddress: document.getElementById('toAddress').value,
+            fromAddress: document.getElementById('fromAddress').value,
+            subject: document.getElementById('subject').value,
+            html: document.getElementById('html').value,
+            text: document.getElementById('text').value
+        }
     }
-}
+    
+    function sendMail() {
+        const emailDataJSON = JSON.stringify(collectEmailData())
+        postRequest(emailDataJSON)
+    }
 
-function sendMail() {
-    const emailDataJSON = JSON.stringify(collectEmailData())
-    postRequest(emailDataJSON)
-}
-
-function initEmailPreview() {
-    const emailPreview = document.getElementById('emailPreview')
-    const emailHTML = document.getElementById('html')
-    emailHTML.addEventListener("change", () => {
-        emailPreview.srcdoc = emailHTML.value
-    })
-}
-
-initEmailPreview()
+    // Initialize form
+    (function initFormAction() {
+        const emailForm = document.getElementById('emailForm')
+        emailForm.onsubmit = sendMail
+    })();
+    
+    (function initEmailPreview() {
+        const emailPreview = document.getElementById('emailPreview')
+        const emailHTML = document.getElementById('html')
+        emailHTML.addEventListener("change", () => {
+            emailPreview.srcdoc = emailHTML.value
+        })
+    })();
+})();
