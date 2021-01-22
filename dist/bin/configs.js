51c435628f54551e9fea93010cc1e46cfc3c3990
CLARENCE\john.jamison <jackofthejamison@hotmail.com>
2021-01-22 11:55:44 +0000

    Unhiding configs.js and adding file to hide for local configs

--- dist/bin/configs.js
+++ dist/bin/configs.js
@@ -1,3 +1,14 @@
 /*
-    It has been hidden from Git by commiting an empty file and then using the command 'git update-index --skip-worktree dist\bin\configs.js'
+    It has been hidden from Git localy by commiting an empty file and then using skip work tree
+    
+    Hide:
+        'git update-index --skip-worktree dist\bin\configs.js'
+
+    Unhide:
+        'git update-index --no-skip-worktree dist\bin\configs.js'
+
 */
+
+const configs = {
+
+}
